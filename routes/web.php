69d91d53a9cf2ed2c9058a8fa06945718921747b
miguel.castaneda <miguel.castaneda@parservicios.com>
2017-08-10 14:15:37 -0500

    creacion de codigo para session

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id' => '3',
+        //'redirect_uri' => 'http://phoneup.client.dev/',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect('http://phoneup.api.dev/oauth/authorize?'.$query);
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
